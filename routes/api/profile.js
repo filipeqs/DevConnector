759234e8ec1d4d3f81c40bb1686e7f3215ed686f
Filipe Silva <filipeqs@outlook.com>
2021-04-01 12:34:36 -0400

    add and delete education from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -195,4 +195,59 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     }
 });
 
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+    '/education',
+    [
+        auth,
+        [
+            check('school', 'School is required.').not().isEmpty(),
+            check('degree', 'Degree is required.').not().isEmpty(),
+            check('fieldofstudy', 'Field of study date is required.').not().isEmpty(),
+            check('from', 'From date is required.').not().isEmpty(),
+        ],
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).send({ errors: errors.array() });
+
+        const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+
+        const newEdu = { school, degree, fieldofstudy, from, to, current, description };
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+
+            profile.education.unshift(newEdu);
+
+            await profile.save();
+
+            return res.send(profile);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send('Server error');
+        }
+    },
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.education = profile.education.filter((item) => item.id !== req.params.edu_id);
+
+        await profile.save();
+
+        return res.send(profile);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
