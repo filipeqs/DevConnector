20132972075727db34834737e95466d47a8fc0d4
Filipe Silva <filipeqs@outlook.com>
2021-04-05 11:05:01 -0400

    register request with axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -12,13 +13,24 @@ const Register = () => {
 
     const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
 
         if (password !== password2) {
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password,
+            };
+
+            try {
+                const res = await axios.post('api/users', newUser);
+                console.log(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
     };
 
