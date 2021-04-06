750f825a57434b4ac30cedfb9ca6e9064aff6e68
Filipe Silva <filipeqs@outlook.com>
2021-04-06 10:47:42 -0400

    logout and navbar links

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -7,6 +7,7 @@ import {
     REGISTER_FAIL,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    LOGOUT,
 } from './types';
 import setAuthToken from '../../utils/setAuthToken';
 
@@ -78,4 +79,11 @@ export const login = (email, password) => async (dispatch) => {
             type: LOGIN_FAIL,
         });
     }
+};
+
+// Logout / Clear Profile
+export const logout = () => (dispatch) => {
+    dispatch({
+        type: LOGOUT,
+    });
 };
