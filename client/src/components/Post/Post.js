46761d7b7cba8b56f8561743025f1393612e03b0
Filipe Silva <filipeqs@outlook.com>
2021-04-09 17:50:12 -0400

    display comments

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,13 +1,14 @@
 import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Spinner from '../layout/Spinner';
+import CommentForm from './CommentForm';
+import PostItem from '../posts/PostItem';
+import CommentItem from './CommentItem';
 
 import { getPost } from '../../redux/actions/post';
-import PostItem from '../posts/PostItem';
-import { Link } from 'react-router-dom';
-import CommentForm from './CommentForm';
 
 const Post = ({ post: { loading, post }, getPost, match }) => {
     useEffect(() => {
@@ -23,6 +24,11 @@ const Post = ({ post: { loading, post }, getPost, match }) => {
             </Link>
             <PostItem post={post} showActions={false} />
             <CommentForm postId={post._id} />
+            <div className="comments">
+                {post.comments.map((comment) => (
+                    <CommentItem key={comment._id} comment={comment} postId={post._id} />
+                ))}
+            </div>
         </Fragment>
     );
 };
